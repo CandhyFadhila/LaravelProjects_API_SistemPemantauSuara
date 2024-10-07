97d43eb403b10463a925913bd59a400bd1abb1ab
CandhyFadhila <omaplocandhy@gmail.com>
2024-10-07 17:19:56 +0700

    UPDATE: Jumlah suara pada map

--- app/Http/Controllers/Dashboard/DetailMapController.php
+++ app/Http/Controllers/Dashboard/DetailMapController.php
@@ -200,7 +200,7 @@ class DetailMapController extends Controller
 
             $format_chart = $suaraKPU->where('kelurahan_id', $firstKelurahanId)->groupBy('partai_id')->map(function ($items) {
                 $partai = $items->first()->partais;
-                $totalSuara = $items->sum('jumlah_suara');
+                $jumlah_suara = $items->first()->jumlah_suara;
 
                 return [
                     'partai' => [
@@ -208,7 +208,7 @@ class DetailMapController extends Controller
                         'nama' => $partai->nama,
                         'color' => $partai->color ?? null
                     ],
-                    'jumlah_suara' => $totalSuara
+                    'jumlah_suara' => $jumlah_suara,
                 ];
             })->values();
 
