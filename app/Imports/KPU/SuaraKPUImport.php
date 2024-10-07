b465f9fef8545e43daa2a6518fd2f9a0be7b143d
rezahawari <rezahawari19@gmail.com>
2024-10-07 09:01:32 +0700

    UPDATE: Fixing Import Suara

--- app/Imports/KPU/SuaraKPUImport.php
+++ app/Imports/KPU/SuaraKPUImport.php
@@ -11,7 +11,7 @@ use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithValidation;
 
-class SuaraKPUImport implements ToModel, WithHeadingRow, WithValidation
+class SuaraKPUImport implements ToModel, WithHeadingRow
 {
     use Importable;
 
@@ -26,56 +26,51 @@ class SuaraKPUImport implements ToModel, WithHeadingRow, WithValidation
         $this->KategoriSuara = KategoriSuara::select('id', 'label')->get();
     }
 
-    public function rules(): array
-    {
-        return [
-            'partai' => 'required',
-            'kelurahan' => 'required',
-            'tahun' => 'required|integer',
-            'tps' => 'required|integer',
-            'kategori_suara' => 'required',
-            'cakupan_wilayah' => 'required',
-            'alamat' => 'required',
-            'jumlah_suara' => 'required|integer',
-            'dpt_laki' => 'required|integer',
-            'dpt_perempuan' => 'required|integer',
-            'jumlah_dpt' => 'required|integer',
-            'suara_caleg' => 'nullable|integer',
-            'suara_partai' => 'nullable|integer',
-        ];
-    }
+    // public function rules(): array
+    // {
+    //     return [
+    //         'partai' => 'required',
+    //         'kelurahan' => 'required',
+    //         'tahun' => 'required|integer',
+    //         'tps' => 'required|integer',
+    //         'kategori_suara' => 'required',
+    //         'cakupan_wilayah' => 'required',
+    //         'alamat' => 'required',
+    //         'jumlah_suara' => 'required|integer',
+    //         'dpt_laki' => 'required|integer',
+    //         'dpt_perempuan' => 'required|integer',
+    //         'jumlah_dpt' => 'required|integer',
+    //         'suara_caleg' => 'nullable|integer',
+    //         'suara_partai' => 'nullable|integer',
+    //     ];
+    // }
 
-    public function customValidationMessages()
-    {
-        return [
-            'partai.required' => 'Nama partai tidak boleh kosong.',
-            'kelurahan.required' => 'Nama kelurahan tidak boleh kosong.',
-            'tahun.required' => 'Tahun tidak boleh kosong.',
-            'tahun.integer' => 'Tahun harus berupa angka.',
-            'tps.required' => 'TPS tidak boleh kosong.',
-            'tps.integer' => 'TPS harus berupa angka.',
-            'cakupan_wilayah.required' => 'Cakupan wilayah TPS tidak boleh kosong.',
-            'kategori_suara.required' => 'Kategori suara TPS tidak boleh kosong.',
-            'alamat.required' => 'Alamat tidak boleh kosong.',
-            'jumlah_suara.required' => 'Jumlah suara tidak boleh kosong.',
-            'jumlah_suara.integer' => 'Jumlah suara harus berupa angka.',
-            'dpt_laki.required' => 'Jumlah DPT laki-laki tidak boleh kosong.',
-            'dpt_perempuan.required' => 'Jumlah DPT perempuan tidak boleh kosong.',
-            'jumlah_dpt.required' => 'Jumlah DPT tidak boleh kosong.',
-            'jumlah_dpt.integer' => 'Jumlah DPT harus berupa angka.',
-            'suara_caleg.integer' => 'Suara caleg harus berupa angka.',
-            'suara_partai.integer' => 'Suara partai harus berupa angka.',
-        ];
-    }
+    // public function customValidationMessages()
+    // {
+    //     return [
+    //         'partai.required' => 'Nama partai tidak boleh kosong.',
+    //         'kelurahan.required' => 'Nama kelurahan tidak boleh kosong.',
+    //         'tahun.required' => 'Tahun tidak boleh kosong.',
+    //         'tahun.integer' => 'Tahun harus berupa angka.',
+    //         'tps.required' => 'TPS tidak boleh kosong.',
+    //         'tps.integer' => 'TPS harus berupa angka.',
+    //         'cakupan_wilayah.required' => 'Cakupan wilayah TPS tidak boleh kosong.',
+    //         'kategori_suara.required' => 'Kategori suara TPS tidak boleh kosong.',
+    //         'alamat.required' => 'Alamat tidak boleh kosong.',
+    //         'jumlah_suara.required' => 'Jumlah suara tidak boleh kosong.',
+    //         'jumlah_suara.integer' => 'Jumlah suara harus berupa angka.',
+    //         'dpt_laki.required' => 'Jumlah DPT laki-laki tidak boleh kosong.',
+    //         'dpt_perempuan.required' => 'Jumlah DPT perempuan tidak boleh kosong.',
+    //         'jumlah_dpt.required' => 'Jumlah DPT tidak boleh kosong.',
+    //         'jumlah_dpt.integer' => 'Jumlah DPT harus berupa angka.',
+    //         'suara_caleg.integer' => 'Suara caleg harus berupa angka.',
+    //         'suara_partai.integer' => 'Suara partai harus berupa angka.',
+    //     ];
+    // }
 
     public function model(array $row)
     {
-        $partai = $this->Partai->where('nama', $row['partai'])->first();
-        if (!$partai) {
-            throw new \Exception("Partai '" . $row['partai'] . "' tidak ditemukan.");
-        }
-
-        $kelurahan = $this->Kelurahan->where('nama_kelurahan', $row['kelurahan'])->first();
+        $kelurahan = $this->Kelurahan->where('nama_kelurahan', ucwords(strtolower($row['kelurahan'])))->first();
         if (!$kelurahan) {
             throw new \Exception("Kelurahan '" . $row['kelurahan'] . "' tidak ditemukan.");
         }
@@ -84,21 +79,52 @@ class SuaraKPUImport implements ToModel, WithHeadingRow, WithValidation
         if (!$kategori_suara) {
             throw new \Exception("Kategori '" . $row['kategori_suara'] . "' tidak ditemukan.");
         }
+        // throw new \Exception($row['total_dpt']);
+        for ($i=0; $i < 18; $i++) {
+            $key = $i;
+            if($i < 18){
+                $key = $i+1;
+            }else {
+                $key = $i;
+            }
+            $partai = $this->Partai->where('nama', $row['partai_'.$key])->first();
+            if (!$partai) {
+                throw new \Exception("Partai '" . $row['partai_'.$key] . "' tidak ditemukan.");
+            }
+            // $partai [] = $row['partai_' . $key];
+            SuaraKPU::create([
+                'partai_id' => $partai->id,
+                'kelurahan_id' => $kelurahan->id,
+                'tahun' => $row['tahun'],
+                'tps' => $row['tps'],
+                'kategori_suara_id' => $kategori_suara->id,
+                'cakupan_wilayah' => $row['cakupan_wilayah'],
+                'alamat' => $row['alamat'],
+                'jumlah_suara' => $row['total_suara_' . $key],
+                'dpt_laki' => $row['dpt_laki'],
+                'dpt_perempuan' => $row['dpt_perempuan'],
+                'jumlah_dpt' => $row['total_dpt'],
+                'suara_caleg' => $row['suara_caleg_' . $key] ?? null,
+                'suara_partai' => $row['suara_partai_' . $key] ?? null,
+            ]);
+            // return new SuaraKPU([
+            //     'partai_id' => $partai->id,
+            //     'kelurahan_id' => $kelurahan->id,
+            //     'tahun' => $row['tahun'],
+            //     'tps' => $row['tps'],
+            //     'kategori_suara_id' => $kategori_suara->id,
+            //     'cakupan_wilayah' => $row['cakupan_wilayah'],
+            //     'alamat' => $row['alamat'],
+            //     'jumlah_suara' => $row['total_suara_'.$key],
+            //     'dpt_laki' => $row['dpt_laki'],
+            //     'dpt_perempuan' => $row['dpt_perempuan'],
+            //     'jumlah_dpt' => $row['total_dpt'],
+            //     'suara_caleg' => $row['suara_caleg_'.$key] ?? null,
+            //     'suara_partai' => $row['suara_partai_'.$key] ?? null,
+            // ]);
+        }
+        // $string = implode(', ', $partai);
+        // throw new \Exception($string);
 
-        return new SuaraKPU([
-            'partai_id' => $partai->id,
-            'kelurahan_id' => $kelurahan->id,
-            'tahun' => $row['tahun'],
-            'tps' => $row['tps'],
-            'kategori_suara_id' => $kategori_suara->id,
-            'cakupan_wilayah' => $row['cakupan_wilayah'],
-            'alamat' => $row['alamat'],
-            'jumlah_suara' => $row['jumlah_suara'],
-            'dpt_laki' => $row['dpt_laki'],
-            'dpt_perempuan' => $row['dpt_perempuan'],
-            'jumlah_dpt' => $row['jumlah_dpt'],
-            'suara_caleg' => $row['suara_caleg'] ?? null,
-            'suara_partai' => $row['suara_partai'] ?? null,
-        ]);
     }
 }
