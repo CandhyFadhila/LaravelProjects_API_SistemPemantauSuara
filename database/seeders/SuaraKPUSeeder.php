e25fb26ee9afae3f419089f7136709ff85c1560d
CandhyFadhila <omaplocandhy@gmail.com>
2024-10-07 14:40:58 +0700

    UPDATE: Suara KPU seeder

--- database/seeders/SuaraKPUSeeder.php
+++ database/seeders/SuaraKPUSeeder.php
@@ -11,27 +11,29 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class SuaraKPUSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        for ($i = 1; $i <= 10; $i++) {
-            SuaraKPU::create([
-                'partai_id' => Partai::inRandomOrder()->first()->id,
-                'kelurahan_id' => Kelurahan::inRandomOrder()->first()->id,
-                'tahun' => 2024,
-                'tps' => rand(1, 10),
-                'kategori_suara_id' => KategoriSuara::inRandomOrder()->first()->id,
-                'alamat' => 'Jalan Jalan',
-                'cakupan_wilayah' => 'Cakupan wilayah dari import',
-                'jumlah_suara' => rand(100, 500),
-                'dpt_laki' => rand(500, 1000),
-                'dpt_perempuan' => rand(500, 1000),
-                'jumlah_dpt' => rand(500, 1000),
-                'suara_caleg' => rand(50, 250),
-                'suara_partai' => rand(50, 250),
-            ]);
+        $kelurahanIds = Kelurahan::pluck('id')->take(10)->toArray();
+        $partaiIds = Partai::pluck('id')->toArray();
+
+        foreach ($kelurahanIds as $kelurahanId) {
+            foreach ($partaiIds as $partaiId) {
+                SuaraKPU::create([
+                    'partai_id' => $partaiId,
+                    'kelurahan_id' => $kelurahanId,
+                    'tahun' => 2024,
+                    'tps' => rand(1, 10),
+                    'kategori_suara_id' => 2,
+                    'alamat' => 'Jalan Jalan',
+                    'cakupan_wilayah' => 'Cakupan wilayah dari import',
+                    'jumlah_suara' => rand(100, 500),
+                    'dpt_laki' => rand(500, 1000),
+                    'dpt_perempuan' => rand(500, 1000),
+                    'jumlah_dpt' => rand(1000, 2000),
+                    'suara_caleg' => rand(50, 250),
+                    'suara_partai' => rand(50, 250),
+                ]);
+            }
         }
     }
 }
